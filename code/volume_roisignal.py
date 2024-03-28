72839bc912c877caaa8c53787e41e6d375944336
Michael Demidenko <36335002+demidenm@users.noreply.github.com>
2024-03-27 18:01:31 -0700

    mods

--- code/volume_roisignal.py
+++ code/volume_roisignal.py
@@ -4,52 +4,49 @@ import numpy as np
 from nilearn.maskers import NiftiLabelsMasker, NiftiSpheresMasker
 
 
-def trlocked_events(events_path: str, onsets_column: str, condition_column: str,
-                    tr: float, volumes: int, separator: str = '\t'):
+def trlocked_events(events_path: str, onsets_column: str, trial_name: str,
+                    bold_tr: float, bold_vols: int, separator: str = '\t'):
     """
-    Loads behavior data, creates and merges into a TR (rounded) dataframe.
+    Loads behavior data, creates and merges into a TR (rounded) dataframe to match length of BOLD. Trial onsets are
+    matched to nearby TR using rounding when acquisition is not locked to TR.
 
     Parameters:
-        events_path (str): Path to the events data file.
-        onsets_column (str): Name of the column containing event onsets.
-        condition_column (str): Name of the column containing event conditions.
-        tr (int): Time resolution in seconds.
-        volumes (int): Number of time points (BOLD volumes).
+        events_path (str): Path to the events data files for given subject/run.
+        onsets_column (str): Name of the column containing onset times for the event/condition.
+        trial_name (str): Name of the column containing condition/trial labels.
+        bold_tr (int): TR acquisition of BOLD in seconds.
+        bold_vols (int): Number of time points for BOLD acquisition
         separator (str): Separator used in the events data file, default = '\t'.
     Returns:
-        pandas.DataFrame: Merged dataframe with time index and events data.
-    Raises:
-        FileNotFoundError: If the events data file is not found.
-        KeyError: If any of the specified columns are missing in events file path.
-        ValueError: If the final length of the merged data doesn't match the volumes provided.
+        pandas.DataFrame: Merged dataframe with time index and events data for each event + TR delays.
     """
     if not os.path.exists(events_path):
         raise FileNotFoundError(f"File '{events_path}' not found.")
 
     beh_df = pd.read_csv(events_path, sep=separator)
 
-    missing_cols = [col for col in [onsets_column, condition_column] if col not in beh_df.columns]
+    missing_cols = [col for col in [onsets_column, trial_name] if col not in beh_df.columns]
     if missing_cols:
         raise KeyError(f"Missing columns: {', '.join(missing_cols)}")
 
-    beh_df = beh_df[[onsets_column, condition_column]]
-    beh_df["TimePoint"] = (beh_df[onsets_column] / tr).round()
+    beh_df = beh_df[[onsets_column, trial_name]]
+    beh_df["TimePoint"] = (beh_df[onsets_column] / bold_tr).round()
 
-    time_index = pd.RangeIndex(start=0, stop=volumes, step=1)
+    time_index = pd.RangeIndex(start=0, stop=bold_vols, step=1)
     time_index_df = pd.DataFrame(index=time_index)
     # Merge behavior data with time index
     merged_df = pd.merge(time_index_df, beh_df, how='left', left_index=True, right_on='TimePoint')
 
-    if len(merged_df) != volumes:
-        raise ValueError(f"Merged data length ({len(merged_df)}) doesn't match volumes ({volumes}).")
+    if len(merged_df) != bold_vols:
+        raise ValueError(f"Merged data length ({len(merged_df)}) doesn't match volumes ({bold_vols}).")
 
     return merged_df
 
 
 def extract_time_series_values(behave_df: pd.DataFrame, time_series_array: np.ndarray, delay: int):
     """
-    Extracts time series data from the provided array for each time point in the given behavioral DataFrame,
-    with a specified delay.
+    Extracts time series data from the provided timeseries BOLD area for associated behavioral data
+    that is acuiqred from trlocked_events w/ specified delay
 
     Parameters:
         behave_df (pandas.DataFrame): DataFrame containing behavioral data with a 'TimePoint' column
@@ -71,24 +68,26 @@ def extract_time_series_values(behave_df: pd.DataFrame, time_series_array: np.nd
 
 
 def extract_time_series(bold_paths: list, roi_type: str, high_pass_sec: int, roi_mask: str = None,
-                        roi_coords: tuple = None, radius_mm: int = None, tr: float = None, detrend=True,
+                        roi_coords: tuple = None, radius_mm: int = None, bold_tr: float = None, detrend=True,
                         fwhm_smooth: float = None):
     """
-    Extract time series data based on the ROI type.
+    For each BOLD path, extract timeseries for either a specified mask or ROI coordinate. Mask and coordinate should be
+    in same space/affine as BOLD data. Function leverages NiftiLabelsMasker (mask path) NiftiSpheresMasker (coordinates)
+    to achieve this.
 
     Parameters:
-        bold_paths (list): List of paths to subsets (list should match for subs/runs/tasks for events file list).
+        bold_paths (list): List of paths to subjects (list should match order of subs/runs/tasks for events file list).
         roi_type (str): Type of ROI ('mask' or 'coordinates').
-        high_pass_sec (int): High-pass filter to use, in seconds. Will be converted to 1/secs.
+        high_pass_sec (int): High-pass filter to use, in seconds. Used to convert to filter freq using 1/secs.
         roi_mask (str or None): Path to the ROI mask image (required if roi_type is 'mask').
         roi_coords (tuple or None): Coordinates (x,y,z) for the sphere center (required if roi_type is 'coordinates').
         radius_mm (int or None): Radius of the sphere in mm (required if roi_type is 'coordinates').
-        tr (float or None): TR (Repetition Time) value.
-        detrend: True/False, whether to use nilearns detrend function.
-        fwhm_smooth (float or None): FWHM (Full Width at Half Maximum) for spatial smoothing.
+        bold_tr (float or None): TR value for acquisition of BOLD data.
+        detrend: True/False, whether to use Nilearn's detrend function.
+        fwhm_smooth (float or None): FWHM for spatial smoothing of data.
 
     Returns:
-        list: List of time series data arrays.
+        list: List of time series for provided subjects/runs.
     """
     if roi_type not in ['mask', 'coordinates']:
         raise ValueError("Invalid ROI type. Choose 'mask' or 'coordinates'.")
@@ -96,7 +95,7 @@ def extract_time_series(bold_paths: list, roi_type: str, high_pass_sec: int, roi
     if roi_type == 'mask':
         masker = NiftiLabelsMasker(labels_img=roi_mask, standardize='psc', resampling_target='data',
                                    detrend=detrend, high_pass=1/high_pass_sec,
-                                   t_r=tr, smoothing_fwhm=fwhm_smooth)
+                                   t_r=bold_tr, smoothing_fwhm=fwhm_smooth)
         time_series = [masker.fit_transform(i) for i in bold_paths]
         return time_series
 
@@ -104,24 +103,26 @@ def extract_time_series(bold_paths: list, roi_type: str, high_pass_sec: int, roi
         masker_coord = NiftiSpheresMasker(seeds=[roi_coords], radius=radius_mm,
                                           standardize='psc', resampling_target='data',
                                           detrend=detrend, high_pass=1/high_pass_sec,
-                                          t_r=tr, smoothing_fwhm=fwhm_smooth)
+                                          t_r=bold_tr, smoothing_fwhm=fwhm_smooth)
         time_series_coord = [masker_coord.fit_transform(i) for i in bold_paths]
         return time_series_coord
 
 
 def extract_postcue_trs_for_conditions(events_data: list, onset: str, trial_name: str,
-                                       tr: float, volumes: int, time_series: np.ndarray,
+                                       bold_tr: float, bold_vols: int, time_series: np.ndarray,
                                        conditions: list, tr_delay: int):
     """
-    Extract TR coinciding with condition onset, plus TRs for specified delay for each file.
+    Extract TR coinciding with condition onset, plus TRs for specified delay for each file. Save this to a pandas
+    dataframe (long) with associated Mean Signal value, for each subject, trial of condition and cue across the range
+    of TRs (1 to TR delay)
 
     Parameters:
         events_data (list): List of paths to behavioral data files (list should match order for
         subs/runs/tasks as bold file list).
         onset (str): Name of the column containing onset values in the behavioral data.
         trial_name (str): Name of the column containing condition values in the behavioral data.
-        tr (int): TR (Repetition Time) value.
-        volumes (int): Number of volumes.
+        bold_tr (int): TR for acquisiton of BOLD data.
+        bold_vols (int): Number of volumes for BOLD.
         time_series (numpy.ndarray): numpy array of time series data.
         conditions (list): List of cue conditions to iterate over, min 1.
         tr_delay (int): Number of TRs to serve as delay (post onset)
@@ -135,7 +136,7 @@ def extract_postcue_trs_for_conditions(events_data: list, onset: str, trial_name
         out_trs = []
         for index, beh_path in enumerate(events_data):
             subset_df = trlocked_events(events_path=beh_path, onsets_column=onset,
-                                        condition_column=trial_name, tr=tr, volumes=volumes, separator='\t')
+                                        trial_name=trial_name, bold_tr=bold_tr, bold_vols=bold_vols, separator='\t')
             trial_type = subset_df[subset_df[trial_name] == cue]
 
             out = extract_time_series_values(behave_df=trial_type, time_series_array=time_series[index], delay=tr_delay)
